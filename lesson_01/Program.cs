63c4bf34a85a41cd1dfc2f4c9539bd1b15abdb83
Evgeny T <eat7381@gmail.com>
2023-10-19 10:55:12 +0300

    Добавил 1 семинар по c#

---
+++ lesson_01/Program.cs
@@ -0,0 +1,186 @@
+﻿// See https://aka.ms/new-console-template for more information    семинарт1
+// Console.WriteLine("Hello, World!");
+
+// Напишите программу,которая на вход,
+// Принимает число и выдает его квадрат (умноженное
+// на само себя)
+
+// Например:
+// 4 - > 16
+// -3 -> 9
+// -7 -> 49
+
+//  Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+//  int result = num * num;
+
+// Console.WriteLine(result);
+
+
+
+// Console.WriteLine("Введите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 == num2 * num2)
+// {
+// Console.WriteLine("yes");
+// }
+
+// else
+// {
+//  Console.WriteLine("no");  
+// }
+
+// Console.WriteLine("Введите число");
+//  int num = Convert.ToInt32(Console.ReadLine());
+
+//  if(num == 1)
+//  {
+//     Console.WriteLine("Понедельник");
+//  }
+//  else if (num == 2)
+//  {
+//     Console.WriteLine("Вторник");
+//  }
+// else if (num == 3)
+
+// {
+//    Console.WriteLine("Среда"); 
+// }
+// else if (num == 4)
+// {
+//   Console.WriteLine("Четверг");   
+// }
+// else if (num == 5)
+// {
+//    Console.WriteLine("Пятница");  
+// }
+// else if (num == 6)
+// {
+//   Console.WriteLine("Суббoта");    
+// }
+// else if (num == 7)
+// {
+//  Console.WriteLine("Воскресенье");    
+// }   
+// else
+// {
+//    Console.WriteLine("Введите другое число");   
+// }
+
+//?
+
+//  Console.WriteLine("Введите число");
+//  int num = Convert.ToInt32(Console.ReadLine());
+
+//  int result = num % 10;
+//  Console.WriteLine("Нужное число" + result + "пнгр");  // Канкатинация
+//  Console.WriteLine($"Нужное число  {result}  пнгр");  // Интраполяция
+
+
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num < 0)
+// {
+//   Console.WriteLine(num); 
+//   num = num + 1;
+// }
+
+// int negNum = num * -1;
+
+// while (negNum<=num)
+
+// {
+// Console.WriteLine(negNum);
+// negNum = negNum+1;
+//  }
+  
+
+// домашка 1
+
+// Внутри класса Answer напишите метод CompareNumbers, 
+// который на вход принимает два числа и выводит, какое число большее, а какое меньшее.
+
+
+//  Console.WriteLine("Введите первое число");              
+//  int a = Convert.ToInt32(Console.ReadLine());  
+
+//  Console.WriteLine("Введите второе число");
+//  int b = Convert.ToInt32(Console.ReadLine());
+
+//  if(a>b)
+//  {
+// Console.WriteLine("Первое число больше второго");
+//  }
+
+//  else
+//  {
+// Console.WriteLine("Второе число больше первого");
+//  }
+
+//  if(a == b)
+//  {
+//  Console.WriteLine("Числа равны"); 
+//  }
+
+
+// Внутри класса Answer напишите метод FindMax, 
+// который принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+// Console.WriteLine("Введите число");
+// int m1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int m2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int m3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = m1;
+
+// if(m2>max) max = m2;
+
+// if(m3>max) max = m3;
+
+// Console.WriteLine(max); 
+
+// Внутри класса Answer напишите метод PrintEvenNumbers, которая на вход принимает число (number), 
+// а на выходе выводит все чётные числа от 1 до number (включительно), разделеные знаком табуляции.
+
+
+ Console.WriteLine("Введите число");
+ int num = Convert.ToInt32(Console.ReadLine());
+
+ int result = 1;
+
+ while (result<=num)
+ {
+  Console.WriteLine(result + 1);
+  result+=2;
+ }
+
+
+// Внутри класса Answer напишите метод CheckIfEven, который на вход принимает число number и выводит,
+//  является ли число чётным (делится ли оно на два без остатка).
+
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0)
+// {
+// Console.WriteLine("Чётное число"); 
+// }
+// else
+// {
+// Console.WriteLine("Число нечетное");
+// }
+
+
